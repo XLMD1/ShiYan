1940082ea477408c0b3203daac99ad736a005008
XLMD1 <3150527756@qq.com>
2026-05-23 21:55:16 +0800

    feat: add one-click setup script for dependencies

---
+++ setup.bat
@@ -0,0 +1,26 @@
+@echo off
+cd /d "%~dp0"
+echo === Setup Data Analysis System ===
+echo.
+
+echo [1/3] Creating Python virtual environment...
+python -m venv venv
+echo Done.
+
+echo.
+echo [2/3] Installing Python packages...
+"%~dp0venv\Scripts\python.exe" -m pip install -r "%~dp0backend\requirements.txt" -q
+echo Done.
+
+echo.
+echo [3/3] Installing Node.js packages...
+cd /d "%~dp0frontend"
+call npm install
+echo Done.
+
+echo.
+echo ==============================
+echo   Setup complete!
+echo   Now double-click start.bat
+echo ==============================
+pause
